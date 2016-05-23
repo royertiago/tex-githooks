74db0baa3b051627dd714b4cb03c0b354c87489e
Tiago Royer <royertiago@gmail.com>
2016-05-23 15:42:55 -0300

    setup.sh: Corrigido diretório.

--- setup.sh
+++ setup.sh
@@ -3,8 +3,8 @@
 
 installed_hooks=0
 
-for file in githooks/*; do
-    file=${file#githooks/}
+for file in tex-githooks/*; do
+    file=${file#tex-githooks/}
     if [[ $file == *.* ]]; then
         # Special exception: if file contains a dot, then it is not a hook.
         # This allows us to spread hooks through several .sh files.
@@ -13,7 +13,7 @@ for file in githooks/*; do
     if [ -e ".git/hooks/$file" ]; then
         echo Skipping existing hook \""$file"\".
     else
-        ln -s "../../githooks/$file" .git/hooks/
+        ln -s "../../tex-githooks/$file" .git/hooks/
         echo Installed hook \""$file"\" using symlink.
         ((installed_hooks++))
     fi
