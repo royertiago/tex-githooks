88b440f2e29693af13fe20d653a86b6d6cd8613c
Tiago Royer <royertiago@gmail.com>
2016-05-23 22:39:35 -0300

    Unido as verificações citeties, forbidden, refties e verbo-ir num único arquivo.

--- pre-commit.forbidden.sh
+++ pre-commit.forbidden.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
-# Script que detecta "frases proibidas".
+# Script que detecta "expressões proibidas" baseado em expressões regulares.
 
-forbidden_regex='\<é as\>\|\<são a\>' # como em "não é as funções..."
+# Forbidden é um mapa regex - mensagem, que é usado no laço abaixo.
+declare -A forbidden=(
+['\<é [ao]s\>\|\<são [ao]\>']='Possível erro de concordância de número.'
+['[Ii]rá\|[Ii]remos\|[Ii]rão']='Troque ex. "irá computar" por "computará".'
+['\[p. [0-9]*\(\]\|,[^]]*]\)']='Existem números de página, em comandos "\cite", sem ties.
+Considere substituir, por exemplo, "[p. 15]" por "[p.~15]".'
+['\(^\|[^~]\)\\ref']='Considere adicionar espaços rígidos (~) antes de comandos "\ref".'
+)
 
-found_forbidden=0
+
+message_written=0
+forbidden_message=""
+set_forbidden_message() {
+    message_written=0
+    forbidden_message="$@"
+}
 show_forbidden_message() {
-    if ((found_forbidden == 0)); then
+    if ((message_written == 0)); then
         echo -n $(tput setaf 1)
-        echo -n Considere remover as seguintes expressões \"estranhas\":
-        echo $(tput sgr0)
-        (( found_forbidden = 1 ))
+        echo "$forbidden_message"
+        echo -n $(tput sgr0)
+        (( message_written = 1 ))
         (( error = 1 ))
     fi
 }
 
-for file in $modified_files; do
-    offending_lines=$(added_lines "$file" |
-        grep --color=always "$forbidden_regex"
-    )
-    if [ ! -z "$offending_lines" ]; then
-        show_forbidden_message
-        print_line "$file" "$offending_lines"
-    fi
+for regex in "${!forbidden[@]}"; do
+    set_forbidden_message "${forbidden[$regex]}"
+    for file in $modified_files; do
+        offending_lines=$(added_lines "$file" |
+            grep --color=always "$regex"
+        )
+        if [ ! -z "$offending_lines" ]; then
+            show_forbidden_message
+            print_line "$file" "$offending_lines"
+        fi
+    done
 done
